6040293b2db364a239e57e727cd2007c8f86562a
李御豪 <sunsethp9999@gmail.com>
2021-12-09 16:15:37 +0800

    update

---
+++ darknet-master/build-release/CMakeFiles/darknet.dir/DependInfo.cmake
@@ -0,0 +1,158 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CUDA"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/Hsinggu/data/darknet-master/src/activation_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/activation_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/activations.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/activations.c.o"
+  "/home/Hsinggu/data/darknet-master/src/art.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/art.c.o"
+  "/home/Hsinggu/data/darknet-master/src/avgpool_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/avgpool_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/batchnorm_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/batchnorm_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/blas.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/blas.c.o"
+  "/home/Hsinggu/data/darknet-master/src/box.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/box.c.o"
+  "/home/Hsinggu/data/darknet-master/src/captcha.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/captcha.c.o"
+  "/home/Hsinggu/data/darknet-master/src/cifar.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/cifar.c.o"
+  "/home/Hsinggu/data/darknet-master/src/classifier.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/classifier.c.o"
+  "/home/Hsinggu/data/darknet-master/src/coco.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/coco.c.o"
+  "/home/Hsinggu/data/darknet-master/src/col2im.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/col2im.c.o"
+  "/home/Hsinggu/data/darknet-master/src/compare.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/compare.c.o"
+  "/home/Hsinggu/data/darknet-master/src/connected_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/connected_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/conv_lstm_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/conv_lstm_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/convolutional_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/convolutional_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/cost_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/cost_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/cpu_gemm.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/cpu_gemm.c.o"
+  "/home/Hsinggu/data/darknet-master/src/crnn_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/crnn_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/crop_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/crop_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/dark_cuda.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/dark_cuda.c.o"
+  "/home/Hsinggu/data/darknet-master/src/darknet.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/darknet.c.o"
+  "/home/Hsinggu/data/darknet-master/src/data.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/data.c.o"
+  "/home/Hsinggu/data/darknet-master/src/deconvolutional_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/deconvolutional_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/demo.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/demo.c.o"
+  "/home/Hsinggu/data/darknet-master/src/detection_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/detection_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/detector.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/detector.c.o"
+  "/home/Hsinggu/data/darknet-master/src/dice.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/dice.c.o"
+  "/home/Hsinggu/data/darknet-master/src/dropout_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/dropout_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/gaussian_yolo_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/gaussian_yolo_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/gemm.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/gemm.c.o"
+  "/home/Hsinggu/data/darknet-master/src/go.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/go.c.o"
+  "/home/Hsinggu/data/darknet-master/src/gru_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/gru_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/im2col.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/im2col.c.o"
+  "/home/Hsinggu/data/darknet-master/src/image.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/image.c.o"
+  "/home/Hsinggu/data/darknet-master/src/layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/list.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/list.c.o"
+  "/home/Hsinggu/data/darknet-master/src/local_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/local_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/lstm_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/lstm_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/matrix.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/matrix.c.o"
+  "/home/Hsinggu/data/darknet-master/src/maxpool_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/maxpool_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/network.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/network.c.o"
+  "/home/Hsinggu/data/darknet-master/src/nightmare.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/nightmare.c.o"
+  "/home/Hsinggu/data/darknet-master/src/normalization_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/normalization_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/option_list.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/option_list.c.o"
+  "/home/Hsinggu/data/darknet-master/src/parser.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/parser.c.o"
+  "/home/Hsinggu/data/darknet-master/src/region_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/region_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/reorg_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/reorg_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/reorg_old_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/reorg_old_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/representation_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/representation_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/rnn.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/rnn.c.o"
+  "/home/Hsinggu/data/darknet-master/src/rnn_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/rnn_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/rnn_vid.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/rnn_vid.c.o"
+  "/home/Hsinggu/data/darknet-master/src/route_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/route_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/sam_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/sam_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/scale_channels_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/scale_channels_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/shortcut_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/shortcut_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/softmax_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/softmax_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/super.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/super.c.o"
+  "/home/Hsinggu/data/darknet-master/src/swag.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/swag.c.o"
+  "/home/Hsinggu/data/darknet-master/src/tag.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/tag.c.o"
+  "/home/Hsinggu/data/darknet-master/src/tree.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/tree.c.o"
+  "/home/Hsinggu/data/darknet-master/src/upsample_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/upsample_layer.c.o"
+  "/home/Hsinggu/data/darknet-master/src/utils.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/utils.c.o"
+  "/home/Hsinggu/data/darknet-master/src/voxel.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/voxel.c.o"
+  "/home/Hsinggu/data/darknet-master/src/writing.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/writing.c.o"
+  "/home/Hsinggu/data/darknet-master/src/yolo.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/yolo.c.o"
+  "/home/Hsinggu/data/darknet-master/src/yolo_layer.c" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/yolo_layer.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CUDNN"
+  "GPU"
+  "OPENCV"
+  "USE_CMAKE_LIBS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "../3rdparty/stb/include"
+  "/usr/local/cuda-10.1/targets/x86_64-linux/include"
+  "/home/Hsinggu/anaconda3/envs/yolov4/include"
+  "/home/Hsinggu/anaconda3/envs/yolov4/include/opencv"
+  )
+set(CMAKE_DEPENDS_CHECK_CUDA
+  "/home/Hsinggu/data/darknet-master/src/activation_kernels.cu" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/activation_kernels.cu.o"
+  "/home/Hsinggu/data/darknet-master/src/avgpool_layer_kernels.cu" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/avgpool_layer_kernels.cu.o"
+  "/home/Hsinggu/data/darknet-master/src/blas_kernels.cu" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/blas_kernels.cu.o"
+  "/home/Hsinggu/data/darknet-master/src/col2im_kernels.cu" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/col2im_kernels.cu.o"
+  "/home/Hsinggu/data/darknet-master/src/convolutional_kernels.cu" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/convolutional_kernels.cu.o"
+  "/home/Hsinggu/data/darknet-master/src/crop_layer_kernels.cu" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/crop_layer_kernels.cu.o"
+  "/home/Hsinggu/data/darknet-master/src/deconvolutional_kernels.cu" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/deconvolutional_kernels.cu.o"
+  "/home/Hsinggu/data/darknet-master/src/dropout_layer_kernels.cu" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/dropout_layer_kernels.cu.o"
+  "/home/Hsinggu/data/darknet-master/src/im2col_kernels.cu" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/im2col_kernels.cu.o"
+  "/home/Hsinggu/data/darknet-master/src/maxpool_layer_kernels.cu" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/maxpool_layer_kernels.cu.o"
+  "/home/Hsinggu/data/darknet-master/src/network_kernels.cu" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/network_kernels.cu.o"
+  )
+set(CMAKE_CUDA_COMPILER_ID "NVIDIA")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CUDA
+  "CUDNN"
+  "GPU"
+  "OPENCV"
+  "USE_CMAKE_LIBS"
+  )
+
+# The include file search paths:
+set(CMAKE_CUDA_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "../3rdparty/stb/include"
+  "/usr/local/cuda-10.1/targets/x86_64-linux/include"
+  "/home/Hsinggu/anaconda3/envs/yolov4/include"
+  "/home/Hsinggu/anaconda3/envs/yolov4/include/opencv"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/Hsinggu/data/darknet-master/src/http_stream.cpp" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/http_stream.cpp.o"
+  "/home/Hsinggu/data/darknet-master/src/image_opencv.cpp" "/home/Hsinggu/data/darknet-master/build-release/CMakeFiles/darknet.dir/src/image_opencv.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CUDNN"
+  "GPU"
+  "OPENCV"
+  "USE_CMAKE_LIBS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "../3rdparty/stb/include"
+  "/usr/local/cuda-10.1/targets/x86_64-linux/include"
+  "/home/Hsinggu/anaconda3/envs/yolov4/include"
+  "/home/Hsinggu/anaconda3/envs/yolov4/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
